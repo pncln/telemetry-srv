4dfa39a1d8ee69ef71266274275e11605a3d622f
Emil Mammadli <56939463+pncln@users.noreply.github.com>
2024-08-26 12:20:29 +0400

    SAME UPDATE GRANDE

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
 import express from 'express';
 import http from 'http';
 import { Server as socketio } from 'socket.io';
-import events from 'events'
+import events from 'events';
 import { getFirestore, connectFirestoreEmulator, collection, addDoc, setDoc, doc } from "firebase/firestore";
 // import firebase from 'firebase/compat/app';
 // import 'firebase/compat/auth';
-import auth from './apis/firebase.js'
+import auth from './apis/firebase.js';
 
 const app = express();
 const server = http.Server(app);
 const websocket = new socketio(server);
 const localEmitter = new events.EventEmitter();
 
-// firebase.initializeApp({
-//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_FIREBASE_APP_ID
-// });
 const db = getFirestore(app);
 connectFirestoreEmulator(db, 'localhost', 8080);
+
+const interpolate = () => {
+
+}
 
 server.listen(3000, async () => {
     console.log('Listening on port 3000');
